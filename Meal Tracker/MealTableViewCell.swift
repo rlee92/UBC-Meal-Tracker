4cd7980b466a601f59ed31f15c1e5657c3d50ced
Ryan Lee <ryan9206@hotmail.com>
2017-03-20 22:04:05 -0700

    Re-committing old project
    Created by following Apple’s swift tutorial
    
    change details:
    - migrated code to remove compilation error
    - changed project’s name for avoid confusion

---
+++ Meal Tracker/MealTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker(swift tutorial)
+//
+//  Created by Ryan Lee on 2016. 12. 29..
+//  Copyright © 2016년 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
